f56850405a33445a689865b5ca5b6e05241da301
xabiere-designs <xabieredesigns@gmail.com>
2025-11-23 15:57:46 -0500

    standarized terraform modules

--- vpc/c2-generic-variables.tf
+++ vpc/c2-generic-variables.tf
@@ -3,3 +3,15 @@ variable "aws_region" {
   type        = string
   default     = "us-east-1"
 }
+
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type        = string
+  default     = "dev"
+}
+
+variable "business_division" {
+  description = "Business Division in the large organization this Infrastructure is for"
+  type        = string
+  default     = "SAP"
+}
