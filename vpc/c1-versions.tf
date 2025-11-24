887cecf23f17b8ee9e103964614282f0c0306872
xabiere-designs <xabieredesigns@gmail.com>
2025-11-24 13:24:46 -0500

    Add VPC Terraform configuration

--- vpc/c1-versions.tf
+++ vpc/c1-versions.tf
@@ -1,13 +1,13 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.21.0"
     }
   }
 }
 
 provider "aws" {
-    region = var.aws_region
-    profile = "default"  
+  region  = var.aws_region
+  profile = "default"
 }
